746dd77bf31f142d1bd8ff1d99b28f439dac100d
Ray Parker <RayParkerBassPlayer@gmail.com>
2025-11-13 11:55:46 -0500

    Add window icon support for task switcher and window decorations

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,6 +13,7 @@
 #include <QTextStream>
 #include <QFileInfo>
 #include <QCloseEvent>
+#include <QIcon>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
@@ -54,6 +55,16 @@ MainWindow::MainWindow(QWidget *parent)
         "    margin: 4px 8px;"
         "}"
     );
+    
+    QIcon icon = QIcon::fromTheme("unfuck-my-timezone-math");
+    if (icon.isNull())
+    {
+        icon = QIcon("/usr/share/icons/hicolor/256x256/apps/unfuck-my-timezone-math.png");
+    }
+    if (!icon.isNull())
+    {
+        setWindowIcon(icon);
+    }
     
     setupMenuBar();
     
