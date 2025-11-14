365f4231acaf302646d6ffebf9edc0117ce1dfd7
Ray Parker <RayParkerBassPlayer@gmail.com>
2025-11-14 08:47:32 -0500

    Fix wrapper script to use /usr instead of /usr/local

--- unfuckmytimezonemath-wrapper.sh
+++ unfuckmytimezonemath-wrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export QT_PLUGIN_PATH="/usr/local/lib/UnfuckMyTimeZoneMath/plugins"
-export LD_LIBRARY_PATH="/usr/local/lib/UnfuckMyTimeZoneMath:$LD_LIBRARY_PATH"
-exec "/usr/local/lib/UnfuckMyTimeZoneMath/UnfuckMyTimeZoneMath" "$@"
+export QT_PLUGIN_PATH="/usr/lib/UnfuckMyTimeZoneMath/plugins"
+export LD_LIBRARY_PATH="/usr/lib/UnfuckMyTimeZoneMath:$LD_LIBRARY_PATH"
+exec "/usr/lib/UnfuckMyTimeZoneMath/UnfuckMyTimeZoneMath" "$@"
